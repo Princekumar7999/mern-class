f0368311f40ddaa2551a751dfc23934cee38a13e
Princekumar7999 <kumarprince.7999@gmail.com>
2024-06-19 12:12:29 +0530

    committ

--- src/App.js
+++ src/App.js
@@ -3,10 +3,34 @@ import './App.css';
 import { a, b } from './components/Products/Products';
 
 import Products from './components/Products/Products';
-
+import { useState } from 'react';
 
 function App() {
-  console.log(a,b);
+  let [cart,setCart] = useState({});
+  function increaseQuantity(product) {
+    const newCart = { ...cart };
+    if(!newcart[Products.id])
+    {
+      newCart[product.id]={
+        id : product.id,
+        title : ptoduct.title,
+        price : product.price,
+        quantity : 0
+      };
+    }
+      newCart[product.id].quantity++;
+      setcart(newCart);
+    }
+    function decreaseQuantity(product) {
+      const newCart = { ...cart };
+      if (!newCart[product.id]) return;
+      newCart[product.id].quantity -= 1;
+      if (newCart[product.id].quantity <= 0) {
+        delete newCart[product.id];
+      }
+      setCart(newCart);
+    }
+    console.log(a,b);
   return (
     <div className="App">
       
